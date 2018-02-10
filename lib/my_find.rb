2181e015c253c40715a6dc39beca079fdb43b011
Eva Smidt <shevangeline@gmail.com>
2018-02-10 23:50:41 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -2,10 +2,10 @@
   def my_find(collection)
     i = 0
     while i < collection.length
-       #return collection[i] if yield(collection[i])
-     if yield(collection[i])
-     return collection[i]
-   end
+       return collection[i] if yield(collection[i])
+    #  if yield(collection[i])
+     #return collection[i]
+   #end
       i += 1
     end
   end
